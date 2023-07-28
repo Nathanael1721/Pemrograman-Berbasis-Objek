fb50fbecb87c54765e2e86158fdf286b9ed9fe1e
Your Name <you@example.com>
2023-07-28 13:25:19 +0700

    first commit

---
+++ app/src/App.java
@@ -0,0 +1,72 @@
+//Encapsilation
+//Inheritance
+//Overriding =mengindahkan / tidak menghiraukan
+//Overloading
+//Class Abstract
+
+abstract class Peminjaman {
+    void denda(){}
+    void pengembalian(){}
+}
+
+class PBO extends Peminjaman {
+    @Override
+    void denda(){}
+    void pengembalian(){}
+}
+
+class Parent {
+    public int x;
+
+    void print(String tipeAnggota, int bayar){
+        System.out.println("Print Parent");
+    }
+
+    void print(String tipeAnggota){
+        System.out.println("Print Parent");
+    }
+
+    void print(){
+        System.out.println("Print 2");
+    }
+
+    // void print() {
+    //     System.out.println("Print Parent");
+    // }
+}
+
+class A extends Parent {
+    
+    @Override
+    void print(){
+        System.out.println("Print A");
+    }
+
+    void test() {
+        print();
+        x = 5;
+    }
+}
+
+class B extends A {
+    void test2(){
+        print();
+        }
+}
+
+public class App extends Parent { 
+    
+    public static void main(String[] args) throws Exception {
+        Parent parent = new Parent();
+        parent.print();
+        parent.print("Mahasiswa");
+
+        A a = new A();
+        a.print();
+
+        //Peminjaman peminjaman = new Peminjaman() {
+            
+        //};
+        //System.out.println("Hello, World!");
+    }
+}
